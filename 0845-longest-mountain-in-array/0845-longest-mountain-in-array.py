fd636aee71a5e6235b4de4097b59c0864b458bc6
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2022-10-14 23:45:47 +0300

    Time: 503 ms (11.37%), Space: 15.4 MB (16.75%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        result=0
+        for i in range(1,len(arr)-1):
+            if arr[i-1]<arr[i]>arr[i+1]:
+                left=right=i
+                while left>0 and arr[left]>arr[left-1]:
+                    left-=1
+                while right+1<len(arr) and arr[right]>arr[right+1]:
+                    right+=1
+                result=max(result,(right-left+1))
+        return result
+        
