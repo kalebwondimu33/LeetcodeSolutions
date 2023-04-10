b904b0fe65dc39abfee5e51aa775d7a17eaa3f4c
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2023-04-10 11:30:31 +0300

    Time: 81 ms (42.44%), Space: 14.1 MB (92.04%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,9 +1,12 @@
 class Solution:
     def leftRigthDifference(self, nums: List[int]) -> List[int]:
-        result=[]
-        for i in range(len(nums)):
-            left_sum=sum(nums[:i])
-            right_sum=sum(nums[i:])-nums[i]
-            result.append(abs(left_sum-right_sum))
-        return result
+        total=sum(nums)
+        fixed_total=sum(nums)
+        for i,v in enumerate(nums):
+            left_sum=fixed_total-total
+            right_sum=total-v
+            total-=v
+            nums[i]=abs(left_sum-right_sum)
+        return nums
+            
         
