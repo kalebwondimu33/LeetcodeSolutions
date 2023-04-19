f4e965ce29c55ae1f5bdbe80781c6e84d7cf85c2
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2023-04-19 21:11:50 +0300

    Time: 71 ms (26.89%), Space: 14.2 MB (31.00%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -2,16 +2,16 @@ class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
         hashtable={}
         result=[]
-        for i in nums1:
-            for j in range(len(i)-1):
-                hashtable[i[j]]=i[j+1]
-        for i in nums2:
-            for j in range(len(i)-1):
-                if i[j] not in hashtable:
-                    hashtable[i[j]]=i[j+1]
-                else:
-                    hashtable[i[j]]+=i[j+1]
-                    
+        for i,j in nums1:
+            if i in hashtable:
+                hashtable[i]+=j
+            else:
+                hashtable[i]=j
+        for i,j in nums2:
+            if i in hashtable:
+                hashtable[i]+=j
+            else:
+                hashtable[i]=j
         for k,v in hashtable.items():
             result.append([k,v])
         return sorted(result)
