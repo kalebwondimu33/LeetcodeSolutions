f1d40dbe868edeb7302738b7cefdd424e0125241
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2023-04-26 11:59:06 +0300

    Time: 34 ms (71.13%), Space: 13.8 MB (56.70%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,11 +1,4 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        hashtable={}
-        for i in s:
-            hashtable[i]=hashtable.get(i,0)+1
-        temp=hashtable[s[0]]
-        for i in hashtable:
-            if hashtable[i]!=temp:
-                return False
-        return True
+        return True if len(set(Counter(s).values()))==1 else False
         
