ab6fe2d0ce720f98a78cb5ad5db95e1d61b0e810
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2023-04-29 14:46:58 +0300

    Time: 52 ms (5.21%), Space: 16.2 MB (11.26%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -1,11 +1,8 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
-        count1=Counter(word1)
-        count2=Counter(word2)
-        for i in count1:
-            if abs(count1[i]-count2.get(i,0))>3:
-                return False
-        for j in count2:
-            if abs(count1.get(j,0)-count2[j])>3:
+        words=set(word1+word2)
+        for i in words:
+            res=abs(word1.count(i)-word2.count(i))>=4
+            if res:
                 return False
         return True
