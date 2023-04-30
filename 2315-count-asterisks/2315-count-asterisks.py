5f179123a5062303d149b9aa3e1f2f7ae0e7e0bd
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2023-04-30 14:26:57 +0300

    Time: 49 ms (5.06%), Space: 16.2 MB (7.64%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.py
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -1,13 +1,10 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        ans,t = 0,0
+        p,r=False,0
         for i in s:
-            if i=="|":
-                t+=1
-            elif t%2==0:
-                if i=="*":
-                    ans+=1
-        return ans
-            
-            
+            if i=="*" and not p:
+                r+=1
+            elif i=="|":
+                p=not p
+        return r
                 
