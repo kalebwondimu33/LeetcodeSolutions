bb9404d94659a54d2b201bbeeb24862c8e7112dd
kalebwondimu33 <99580734+kalebwondimu33@users.noreply.github.com>
2023-05-03 20:19:31 +0300

    Time: 51 ms (5.13%), Space: 16.3 MB (6.68%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,14 +1,8 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
-        dic={"0":[],"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[]}
-        stack=[]
-        for i in  rings:
-            if i.isdigit():
-                dic[i].append(stack[-1])
-            else:
-                stack.append(i)
         count=0
-        for i in dic:
-            if "B" in dic[i]and "G" in dic[i] and "R" in dic[i]:
+        for i in range(10):
+            i=str(i)
+            if "R"+i in rings and "G"+i in rings and "B"+i in rings:
                 count+=1
         return count
