7b8dbb3a967778fc87791edacbc1dc36b250b1ab
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-06-06 10:40:24 +0300

    Time: 61 ms (80.94%), Space: 16.2 MB (76.96%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.py
+++ 2716-minimize-string-length/2716-minimize-string-length.py
@@ -1,10 +1,5 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        stack=[]
-        for i in s:
-            if stack and i in stack:
-                pass
-            else:
-                stack.append(i)
-        return len(stack)
+        result=set(s)
+        return len(result)
         
