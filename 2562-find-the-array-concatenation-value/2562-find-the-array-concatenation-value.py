6297b13c51d1ee35df1965ab147240d294058911
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-07-02 19:59:58 +0300

    Time: 73 ms (52.18%), Space: 16.6 MB (28.10%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -3,23 +3,16 @@ class Solution:
         con_value=0
         left=0
         right=len(nums)-1
-        if len(nums)%2==0:
-            while left<right:
+        while left<=right:
+            if left==right:
+                add=str(nums[left])
+                con_value+=int(add)
+                left+=1
+                right-=1
+            else:
                 add=str(nums[left])+str(nums[right])
                 con_value+=int(add)
                 left+=1
                 right-=1
-        else:
-            while left<=right:
-                if left==right:
-                    add=str(nums[left])
-                    con_value+=int(add)
-                    left+=1
-                    right-=1
-                else:
-                    add=str(nums[left])+str(nums[right])
-                    con_value+=int(add)
-                    left+=1
-                    right-=1
         return con_value
         
