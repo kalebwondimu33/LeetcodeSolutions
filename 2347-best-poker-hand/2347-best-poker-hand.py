2206f085ad49666131c58b89faab3facfdd09894
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-10-04 21:20:46 +0300

    Time: 39 ms (53.74%), Space: 16.2 MB (87.85%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.py
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -1,32 +1,16 @@
 class Solution:
     def bestHand(self, ranks: List[int], suits: List[str]) -> str:
-        dic_rank={}
-        dic_suits={}
-        for i in ranks:
-            if i not in dic_rank:
-                dic_rank[i]=1
-            else:
-                dic_rank[i]+=1
-        for j in suits:
-            if j not in dic_suits:
-                dic_suits[j]=1
-            else:
-                dic_suits[j]+=1
-        
-        for i in dic_suits:
-            if dic_suits[i]==5:
-                return "Flush"
-        maxx_rank=0
-        for j in dic_rank:
-            if dic_rank[j]>maxx_rank:
-                maxx_rank=dic_rank[j]
-        print(dic_rank)
-        print(maxx_rank)
-        if maxx_rank>=3:
+        if len(set(suits))==1:
+            return "Flush"
+        rank=Counter(ranks).most_common();
+        if rank[0][1]>=3:
             return "Three of a Kind"
-        if maxx_rank==2:
+        elif rank[0][1]==2:
             return "Pair"
-        if maxx_rank==1:
+        else:
             return "High Card"
+        
+        
+        
         
         
