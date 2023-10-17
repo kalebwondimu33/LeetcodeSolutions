2d648e83036d2db012055646358268f77d34d804
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-10-17 23:36:32 +0300

    Time: 61 ms (59.87%), Space: 16.2 MB (68.91%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,12 +1,24 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        i=0
-        while True:
-            if i==len(words)-1:
-                break
-            if ''.join(sorted(words[i]))==''.join(sorted(words[i+1])):
-                words.pop(i+1)
-                i-=1
-            i+=1
-        return words
+        # i=0
+        # while True:
+        #     if i==len(words)-1:
+        #         break
+        #     if ''.join(sorted(words[i]))==''.join(sorted(words[i+1])):
+        #         words.pop(i+1)
+        #         i-=1
+        #     i+=1
+        # return words
+        sorted_num=[sorted(w) for w in words]
+        for i in range(1,len(sorted_num)):
+            if sorted_num[i-1]==sorted_num[i]:
+                words[i]=None
+        result=[]
+        for i in words:
+            if i !=None:
+                result.append(i)
+        return result
+    
+    
+    
         
