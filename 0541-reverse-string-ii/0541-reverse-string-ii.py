50a4033907c87b2f74c984991858e0fd0417513b
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-11-04 21:36:12 +0300

    Time: 48 ms (18.73%), Space: 16.6 MB (15.07%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        if len(s)<(k):return s[::-1]
+        if len(s)<(2*k):return (s[:k][::-1]+s[k:])
+        return s[:k][::-1]+s[k:2*k]+self.reverseStr(s[2*k:],k)
+            
