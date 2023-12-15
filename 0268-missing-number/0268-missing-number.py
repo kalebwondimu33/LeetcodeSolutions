d9d4ed96390feb13e5b97f913b83fd39b417e1ef
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-12-15 19:18:15 +0300

    Time: 121 ms (75.57%), Space: 17.6 MB (89.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        res=len(nums)
-        for i in range(len(nums)):
-            res+=(i-nums[i])
-        return res
-        
+        total1=sum(nums)
+        n=len(nums)
+        alltotal=sum([i for i in range(n+1)])
+        print(alltotal)
+        return alltotal-total1
