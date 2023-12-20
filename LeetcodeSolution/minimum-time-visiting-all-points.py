afbb1157b6a3af7b857a95e5e87cde9e8be92044
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-12-20 19:48:02 +0300

    Added solution for Minimum Time Visiting All Points

---
+++ LeetcodeSolution/minimum-time-visiting-all-points.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        steps = 0
+        for i in range(len(points)-1):
+            point = points[i]
+            next_point = points[i+1]
+            steps += max(abs(next_point[0] - point[0]), abs(next_point[1] - point[1]))
+        return steps
+        
