2b83ac954953f1e2fcce48dde93109baabaee695
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2024-01-01 16:09:42 +0300

    Time: 93 ms (30.03%), Space: 17.5 MB (6.09%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,6 +1,6 @@
 class Solution:
-    def findTheWinner(self, n: int, k: int) -> int:
-        game=sorted(x+1 for x in range(n))
+    def findTheWinner(self, n: int, k: int) -> int:        
+        game=[i for i in range(1,n+1)]
         current=len(game)-1
         while len(game)>1:
             current=(current+k)%len(game)
@@ -9,4 +9,6 @@ class Solution:
             current%=len(game)
         return game[0]
         
+            
+       
         
