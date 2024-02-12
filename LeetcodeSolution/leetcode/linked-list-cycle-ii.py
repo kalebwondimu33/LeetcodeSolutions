9cf6fd7122863883eae2c5e5bc7f651303b125c4
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2024-02-13 00:20:40 +0300

    Add solution for Linked List Cycle II

---
+++ LeetcodeSolution/leetcode/linked-list-cycle-ii.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        if not head.next:
+            return None
+        hare=head
+        tortoise=head
+        while hare and hare.next:
+            hare=hare.next.next
+            tortoise=tortoise.next
+            if hare==tortoise:
+                break
+        if tortoise!=hare:
+            return None
+        pointer=head
+        while pointer!=tortoise:
+            pointer=pointer.next
+            tortoise=tortoise.next
+        return tortoise
